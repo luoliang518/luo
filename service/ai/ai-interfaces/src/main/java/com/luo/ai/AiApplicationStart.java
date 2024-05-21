5504f869a11d67d87a9ffa2f687981eb79711b0e
luoliang <17357170942@163.com>
2024-05-21 17:18:42 +0800

    初始化分支

---
+++ service/ai/ai-interfaces/src/main/java/com/luo/ai/AiApplicationStart.java
@@ -0,0 +1,16 @@
+package com.luo.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description ai服务启动类
+ * @Author luoliang
+ * @Date 2024/5/21
+ */
+@SpringBootApplication
+public class AiApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(AiApplicationStart.class,args);
+    }
+}
