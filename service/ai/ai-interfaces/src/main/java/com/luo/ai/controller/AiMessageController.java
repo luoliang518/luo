62026fa18f488beb989391d8edec03673e97da0c
luoliang <17357170942@163.com>
2024-05-21 17:21:23 +0800

    初始化分支

---
+++ service/ai/ai-interfaces/src/main/java/com/luo/ai/controller/AiMessageController.java
@@ -0,0 +1,15 @@
+package com.luo.ai.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author luoliang
+ * @Date 2024/5/21
+ */
+@RestController
+@RequestMapping("/aiMessage")
+public class AiMessageController {
+
+}
