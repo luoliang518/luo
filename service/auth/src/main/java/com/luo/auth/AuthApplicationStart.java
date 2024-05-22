8ac505df5fe06b912ac5b7614beed05175ec398e
luoliang <1822375984@qq.com>
2024-05-22 23:24:47 +0800

    初始化分支

---
+++ service/auth/src/main/java/com/luo/auth/AuthApplicationStart.java
@@ -0,0 +1,16 @@
+package com.luo.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description auth服务启动类
+ * @Author luoliang
+ * @Date 2024/5/21
+ */
+@SpringBootApplication
+public class AuthApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplicationStart.class,args);
+    }
+}
---
+++ service/auth/src/main/java/com/luo/auth/interfaces/controller/UserAuthController.java
@@ -0,0 +1,13 @@
+package com.luo.auth.interfaces.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author luoliang
+ */
+@RestController
+@RequestMapping("/userAuth")
+public class UserAuthController {
+
+}
