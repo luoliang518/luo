f4e936f23cfd4e80341f3aa251001dd623bf23ef
luoliang <!822375984@qq.com>
2024-06-13 23:26:50 +0800

    加了个banner

--- service/auth/src/main/java/com/luo/auth/AuthApplicationStart.java
+++ service/auth/src/main/java/com/luo/auth/AuthApplicationStart.java
@@ -1,17 +1,32 @@
 package com.luo.auth;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @Description auth服务启动类
  * @Author luoliang
  * @Date 2024/5/21
  */
+@Slf4j
 @SpringBootApplication()
 public class AuthApplicationStart {
-    public static void main(String[] args) {
-        SpringApplication.run(AuthApplicationStart.class,args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(AuthApplicationStart.class, args);
+        Environment env = application.getEnvironment();
+        log.info("\n------------------------------------------------------------\n\t" +
+                        "岁岁年年 平平安安 快快乐乐 : \thttp://{}:{}{}\n" +
+                        "-------------------------------------------------------------",
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                env.getProperty("server.servlet.context-path")
+        );
     }
 }
